530a93e9d0cad4cfb27e0bfaae99c85ff515e61f
Thomas Fahle <tomfahle@cpan.org>
2012-04-07 18:29:19 +0000

    Import of TOMFAHLE/Task-BeLike-TomFahle-0.11 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-TomFahle
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         TOMFAHLE/Task-BeLike-TomFahle-0.11.tar.gz
    gitpan-cpan-author:       TOMFAHLE
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/TomFahle.pm
@@ -0,0 +1,166 @@
+use 5.10.0;
+use warnings;
+use strict;
+
+package Task::BeLike::TomFahle;
+
+our $VERSION = '0.11';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::BeLike::TomFahle
+
+=head1 DESCRIPTION
+
+Just an install package for CPAN modules I frequently use.
+
+=head1 TASKs
+
+=head2 App::Ack
+
+App::Ack L<http://search.cpan.org/perldoc?App::Ack>
+
+=head2 Mozilla::CA
+
+Mozilla::CA L<http://search.cpan.org/perldoc?Mozilla::CA> 
+
+=head2 Perl::Tidy
+
+Perl::Tidy L<http://search.cpan.org/perldoc?Perl::Tidy>
+
+=head2 Task::Kensho
+
+Task::Kensho L<http://search.cpan.org/perldoc?Task::Kensho>
+
+=head3 Task::Kensho::Async
+
+Task::Kensho::Async L<http://search.cpan.org/perldoc?Task::Kensho::Async>
+
+=head3 Task::Kensho::CLI
+
+Task::Kensho::CLI L<http://search.cpan.org/perldoc?Task::Kensho::CLI>
+
+=head3 Task::Kensho::Config
+
+Task::Kensho::Config L<http://search.cpan.org/perldoc?Task::Kensho::Config>
+
+=head3 Task::Kensho::DBDev
+
+Task::Kensho::DBDev L<http://search.cpan.org/perldoc?Task::Kensho::DBDev>
+
+=head3 Task::Kensho::Dates
+
+Task::Kensho::Dates L<http://search.cpan.org/perldoc?Task::Kensho::Dates>
+
+=head3 Task::Kensho::Email
+
+Task::Kensho::Email L<http://search.cpan.org/perldoc?Task::Kensho::Email>
+
+=head3 Task::Kensho::ExcelCSV
+
+Task::Kensho::ExcelCSV L<http://search.cpan.org/perldoc?Task::Kensho::ExcelCSV>
+
+=head3 Task::Kensho::Exceptions
+
+Task::Kensho::Exceptions L<http://search.cpan.org/perldoc?Task::Kensho::Exceptions>
+
+=head3 Task::Kensho::Hackery
+
+Task::Kensho::Hackery L<http://search.cpan.org/perldoc?Task::Kensho::Hackery>
+
+=head3 Task::Kensho::Logging
+
+Task::Kensho::Logging L<http://search.cpan.org/perldoc?Task::Kensho::Logging>
+
+=head3 Task::Kensho::ModuleDev
+
+Task::Kensho::ModuleDev L<http://search.cpan.org/perldoc?Task::Kensho::ModuleDev>
+
+=head3 Task::Kensho::OOP
+
+Task::Kensho::OOP L<http://search.cpan.org/perldoc?Task::Kensho::OOP>
+
+=head3 Task::Kensho::Scalability
+
+Task::Kensho::Scalability L<http://search.cpan.org/perldoc?Task::Kensho::Scalability>
+
+=head3 Task::Kensho::Testing
+
+Task::Kensho::Testing L<http://search.cpan.org/perldoc?Task::Kensho::Testing>
+
+=head3 Task::Kensho::Toolchain
+
+Task::Kensho::Toolchain L<http://search.cpan.org/perldoc?Task::Kensho::Toolchain>
+
+=head3 Task::Kensho::WebCrawling
+
+Task::Kensho::WebCrawling L<http://search.cpan.org/perldoc?Task::Kensho::WebCrawling>
+
+=head3 Task::Kensho::WebDev
+
+Task::Kensho::WebDev L<http://search.cpan.org/perldoc?Task::Kensho::WebDev>
+
+=head3 Task::Kensho::XML
+
+Task::Kensho::XML Task::Kensho::XML L<http://search.cpan.org/perldoc?Task::Kensho::XML>
+
+
+=head1 SEE ALSO
+
+Inspired by
+
+=over 4
+
+=item * Task::BeLike::RJBS
+
+L<http://search.cpan.org/perldoc?Task::BeLike::RJBS>
+
+=item * Task::BeLike::hanekomu
+
+L<http://search.cpan.org/perldoc?Task::BeLike::hanekomu>
+
+=item * Task::BeLike::.*
+
+L<http://search.cpan.org/perldoc?Task::BeLike>
+
+=back
+
+
+=head1 SUPPORT
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-TomFahle>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-BeLike-TomFahle>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-BeLike-TomFahle>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-BeLike-TomFahle/>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012 Thomas Fahle
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
